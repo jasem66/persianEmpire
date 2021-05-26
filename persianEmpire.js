5d741362c460b9445a39b645987bdacda4046841
jasem66 <jasemsarafraz66@gmail.com>
2021-05-26 21:49:42 +0100

    first commit

---
+++ persianEmpire.js
@@ -0,0 +1,30 @@
+let nationality = prompt("please inter your nationality:")
+nationality = nationality.toUpperCase()
+
+switch(nationality){
+case "ENGLISH" :
+    case "EN" :
+    alert("you have a great history")
+    break
+case "PERSIAN" :
+    case "FA" :
+        alert("khosh amadid")
+        break
+        default :
+        alert("we love all natiolities") 
+}
+
+const password = "gabriela"
+ for(let i=1 ; i<=4 ; i++){
+const userinput = prompt("enter your password") 
+if (userinput == password){
+    alert("wellcome to persian empire🤴🤴")
+    break
+}
+    else{
+        alert(`you are not on of our soldiers!👮‍♀️👮‍♂️ ${4-i} more chance!`)
+    }
+}
+
+
+
