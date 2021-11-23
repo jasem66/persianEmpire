dcf73fafed044151beed23b879b71010771be89c
jasem66 <jasemsarafraz66@gmail.com>
2021-11-23 12:22:39 +0200

    Update persianEmpire.js

--- persianEmpire.js
+++ persianEmpire.js
@@ -1,29 +1,41 @@
-let nationality = prompt("please inter your nationality:")
-nationality = nationality.toUpperCase()
+// let nationality = prompt("please inter your nationality:")
+// nationality = nationality.toUpperCase()
 
-switch(nationality){
-case "ENGLISH" :
-    case "EN" :
-    alert("you have a great history")
-    break
-case "PERSIAN" :
-    case "FA" :
-        alert("khosh amadid")
-        break
-        default :
-        alert("we love all nationalities") 
-}
+// switch(nationality){
+// case "ENGLISH" :
+//     case "EN" :
+//     alert("you have a great history")
+//     break
+// case "PERSIAN" :
+//     case "FA" :
+//         alert("khosh amadid")
+//         break
+//         default :
+//         alert("we love all nationalities") 
+// }
 
-const password = "elena"
- for(let i=1 ; i<=4 ; i++){
-const userinput = prompt("enter your password") 
-if (userinput == password){
-    alert("wellcome to persian empire🤴🤴")
-    break
-}
-    else{
-        alert(`you are not on of our soldiers!👮‍♀️👮‍♂️ ${4-i} more chance!`)
-    }
-}
+// const password = "elena"
+//  for(let i=1 ; i<=4 ; i++){
+// const userinput = prompt("enter your password") 
+// if (userinput == password){
+//     alert("wellcome to persian empire🤴🤴")
+//     break
+// }
+//     else{
+//         alert(`you are not on of our soldiers!👮‍♀️👮‍♂️ ${4-i} more chance!`)
+//     }
+// }
 
+// const navToggle = document.querySelector(".navbar-toggler");
+// const links = document.querySelector(".hidden-links");
+
+// navToggle.addEventListener("click", function(){
+//    console.log(links.classList.contains("hidden-links"));
+//    if(links.classList.contains("hidden-links")){
+//        links.classList.remove("hidden-links");
+//    }else{
+//        links.classList.add("show-links");
+//    } 
+//    links.classList.toggle("show-links");
+// });
 
